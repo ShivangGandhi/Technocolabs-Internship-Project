8154ef0e140d9ad061786fc2e25c28ae959bd076
ShivangGandhi <54973764+ShivangGandhi@users.noreply.github.com>
2020-08-01 15:28:24 +0530

    Create main.js

---
+++ Javascript_task/Task_7/main.js
@@ -0,0 +1,29 @@
+//myage 
+let myAge = 20;
+
+//earlyyrs
+let earlyYears = 2;
+
+//multiply by 10.5
+earlyYears = earlyYears * 10.5;
+
+//Since we already accounted for the first two years, take the myAge variable, and subtract 2 from it.
+let laterYears = myAge - 2;
+
+//Multiply the laterYears variable by 4 to calculate the number of dog years accounted for by your later years.
+laterYears *= 4;
+
+//console.log(myAge,earlyYears,laterYears);
+
+//age in dog years
+myAgeInDogYears = earlyYears + laterYears;
+
+
+//storing my name as string
+let myName = "Shivangkumar Gandhi";
+
+//to Lowercase
+myName = myName.toLowerCase();
+
+//printing the conversion of my age to dog-age.
+console.log("My name is " + myName + ". I am " + myAge + " years old in human years which is " + myAgeInDogYears + " years old in dog years.");
